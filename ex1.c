1ec7b0dd273ef5399241cb63bc312ddfa55ee922
efkanefe <efkanefekabakcii@gmail.com>
2024-10-24 14:51:01 +0300

    first assignment is done

--- ex1.c
+++ ex1.c
@@ -1,1 +1,15 @@
-printf("This is a new commit")
+int main(){
+    int secretNumber = 3;
+    int userGuess;
+
+    printf("What's your guess? ");
+    scanf("%d", &userGuess);
+    
+    if (userGuess == secretNumber){
+        printf("Congratulations!");
+    } else {
+        printf("Ooops! You couldn't guess the number :<");
+    }
+
+    return 0;
+}
