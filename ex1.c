3b0017c3e01f1376a86b1582f863ea663ec2bcb0
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2024-11-12 15:08:37 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int guess;
+    printf("Guess a number between 1 and 5: ");
+    scanf("%d", &guess);
+    
+    if (guess == 3) {
+        printf("Correct! You're a great guesser!\n");
+    } else {
+        printf("Not quite! The number was 3.\n");
+    }
+    return 0;
+}
