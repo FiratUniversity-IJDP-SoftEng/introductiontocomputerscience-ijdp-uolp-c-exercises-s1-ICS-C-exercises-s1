281895dc35311d63c163cdaf5d9f40de09f9a948
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2024-11-12 15:09:11 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+
+int main() {
+    char pet[10];
+    printf("Do you have a cat or a dog? ");
+    scanf("%s", pet);
+    
+    if (pet[0] == 'c' || pet[0] == 'C') {
+        printf("Meow meow!\n");
+    } else if (pet[0] == 'd' || pet[0] == 'D') {
+        printf("Woof woof!\n");
+    } else {
+        printf("I don't know that pet!\n");
+    }
+    return 0;
+}
