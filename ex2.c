e798790c68741bda3afebccde0d9e2d12d474ba4
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-10-21 07:23:55 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int age;
-    printf("How old are you? ");
-    scanf("%d", &age);
-    
-    for (int i = 0; i < age; i++) {
-        printf("Happy Birthday!\n");
-    }
-    return 0;
-}
