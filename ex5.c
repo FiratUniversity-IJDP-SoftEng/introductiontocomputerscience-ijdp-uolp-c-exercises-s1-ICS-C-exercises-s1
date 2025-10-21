c30cb113a2ac14fd9bb6d84b74f4911c4ea81234
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-10-21 07:24:39 +0300

    Remove ex5.c file

--- ex5.c
+++ ex5.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
 
-void sayHello(char name[]) {
-    printf("Hello, %s!\n", name);
-}
-
-int main() {
-    char userName[20];
-    printf("What's your name? ");
-    scanf("%s", userName);
-    sayHello(userName);
-    return 0;
-}
