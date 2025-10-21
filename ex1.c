99fe7817586dd75c49a88df8b30a425bc8e49109
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-10-21 07:23:38 +0300

    Remove main function from ex1.c

--- ex1.c
+++ ex1.c
@@ -1,13 +1,1 @@
 
-int main() {
-    int guess;
-    printf("Guess a number between 1 and 5: ");
-    scanf("%d", &guess);
-    
-    if (guess == 3) {
-        printf("Correct! You're a great guesser!\n");
-    } else {
-        printf("Not quite! The number was 3.\n");
-    }
-    return 0;
-}
